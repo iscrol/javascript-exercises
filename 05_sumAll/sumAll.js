55fbc1d72c9925b83849212194723a9c1cde25a9
Ian Scrol <imscrol@gmail.com>
2024-06-13 15:55:55 -0500

    Did exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    if (typeof x !== 'number' || typeof y !== 'number'){
+        return 'ERROR';
+    } else if (Math.min(x,y) < 0) {
+        return 'ERROR';
+    }
 
+    if (x > y) {
+        let temp = x;
+        x = y;
+        y = temp;
+    }
+
+    let sum = 0;
+    for (i = x; i <= y; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
