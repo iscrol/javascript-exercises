861cd88bd7a470c0648e0bd09121893c7ccf51c0
Ian Scrol <imscrol@gmail.com>
2024-06-13 15:28:50 -0500

    Did Excersise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...theArgs) {
+    argLen = theArgs.length;
+    arrLen = arr.length;
+    for (i = 0; i < argLen; i++){
+        for (j = 0; j < arrLen; j++){
+            if (theArgs[i] === arr[j]){
+                arr.splice(j--, 1);
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
